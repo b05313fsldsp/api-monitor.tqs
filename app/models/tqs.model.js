c3b72b27353d5822056da0313d38090802784e99
david <rd02navc2@gmail.com>
2021-10-15 09:35:50 +0800

    the real tqs data format

---
+++ app/models/tqs.model.js
@@ -0,0 +1,25 @@
+//var mongoose = require('mongoose')
+//var SchemaTypes = mongoose.Schema.Types;
+
+module.exports = mongoose => {
+  var schema = mongoose.Schema(
+    {
+      status: String, // title
+      concentration: Number,  // SN
+      temp2: Number,  // CANID
+      tanklevel: Number, // SPN1761
+      tqstimestamps: String, // TTIMESTAMP
+      // createdAt:String
+
+    }
+  );
+
+  schema.method("toJSON", function() {
+    const { __v, _id, ...object } = this.toObject();
+    object.id = _id;
+    return object;
+  });
+
+  const Tqs = mongoose.model("tqs", schema);
+  return Tqs;
+};
